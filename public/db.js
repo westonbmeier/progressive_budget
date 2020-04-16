9205987060642f0e1e0bdbff32a9ef39b783cf9f
westonbmeier <westonbmeier@gmail.com>
2020-04-16 17:45:16 -0600

    pending transactions setup

--- public/db.js
+++ public/db.js
@@ -4,12 +4,15 @@ const request = window.indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
+
     db.createObjectStore("toAdd", {autoIncrement: true});
 }
 
 request.onsuccess = function(event) {
     db = event.target.result;
+
     if (navigator.onLine) {
+
         pendingTransactions();
     }
 }
@@ -18,6 +21,15 @@ request.onerror = function(event) {console.log("Error " + event)};
 
 function saveRecord(dataToAdd) {
     const transaction = db.transaction(["toAdd"], "readwrite");
+
     const store = transaction.objectStore("toAdd");
+
     store.add(dataToAdd);
 }
+
+function pendingTransactions() {
+    const transaction = db.transaction(["toAdd"], "readwrite");
+
+    const store = transaction.objectStore("toAdd");
+    
+    const all = store.getAll();
