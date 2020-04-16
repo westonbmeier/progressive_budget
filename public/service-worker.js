11d41adfa8944aa26fe1925ad2d1ce8dc33fe4cf
westonbmeier <westonbmeier@gmail.com>
2020-04-16 17:56:48 -0600

    event listener for installation

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,3 +9,17 @@
 
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
+
+
+  self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Files have been pre-cached");
+
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
