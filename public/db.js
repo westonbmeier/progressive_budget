7f4c13310aebe9d20cd920b6f1f32497ed32d60f
westonbmeier <westonbmeier@gmail.com>
2020-04-16 17:46:18 -0600

    on success

--- public/db.js
+++ public/db.js
@@ -31,5 +31,18 @@ function pendingTransactions() {
     const transaction = db.transaction(["toAdd"], "readwrite");
 
     const store = transaction.objectStore("toAdd");
-    
+
     const all = store.getAll();
+
+    all.onsuccess = function() {
+        if (all.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+
+                body: JSON.stringify(all.result),
+                
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
