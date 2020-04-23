e118679909bc6454c90a4dc6858c74f456a11f9a
westonbmeier <westonbmeier@gmail.com>
2020-04-22 19:22:07 -0600

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 8080;;
+const PORT = 3000;
 
 const app = express();
 
